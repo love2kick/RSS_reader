e3e3bb77b0fec7da526fbccf8c34f456db2fb116
MOSCOW\Daniil_Bogdanov <daniil_bogdanov@epam.com>
2022-09-07 16:12:57 +0400

    added tests for converter

--- reader/tests/test_converter.py
+++ reader/tests/test_converter.py
@@ -1,17 +1,38 @@
-from converter import convert_to_html
+import os
+from converter import convert_to_html, PDF_converter
+from pathlib import Path
+import os
 
-def test_to_html():
-    test_dict={  
+test_dict={  
                 "content": {  
-                    "feed": ["FeedName"],  
+                    "feed": "TESTFEED",  
                     "items": {  
                         "item0": {  
-                            "title": "TestTitle",  
-                            "date": "1945-05-10",  
-                            "link": "https://www.TEST",  
-                            "description": "SomeRandomDescription"  
+                            "TITLE": "TestTitle",  
+                            "DATE": "1945-05-10",  
+                            "LINK": "https://www.TEST",  
+                            "DESCRIPTION": "SomeRandomDescription"  
                             }  
                         }  
                     }  
                 } 
+
+
+media_path = os.path.join(Path(os.path.dirname(__file__)).parent, 'media')
+
+
+def test_to_html():
     convert_to_html(test_dict)
+    assert Path(os.path.join(media_path, 'TESTFEED.html')).is_file() == True
+    
+    
+def test_to_pdf():
+    PDF_converter(test_dict)
+    assert Path(os.path.join(media_path, 'TESTFEED.pdf')).is_file() == True
+    
+    
+def test_cleanup():
+    os.remove(Path(os.path.join(media_path, 'TESTFEED.html')))
+    os.remove(Path(os.path.join(media_path, 'TESTFEED.pdf')))
+    assert Path(os.path.join(media_path, 'TESTFEED.html')).is_file() == False
+    assert Path(os.path.join(media_path, 'TESTFEED.pdf')).is_file() == False
